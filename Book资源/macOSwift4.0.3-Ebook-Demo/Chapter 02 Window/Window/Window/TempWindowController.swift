a49c37dc7ebca03d19c1e799676de2eeb84d56d1
ma qianli <>
2019-08-10 21:57:47 +0800

    Mac OSX

---
+++ Book资源/macOSwift4.0.3-Ebook-Demo/Chapter 02 Window/Window/Window/TempWindowController.swift
@@ -0,0 +1,23 @@
+//
+//  TempWindowController.swift
+//  Window
+//
+//  Created by ma qianli on 2019/8/10.
+//  Copyright © 2019 http://www.macdev.io. All rights reserved.
+//
+
+import Cocoa
+
+class TempWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+    deinit {
+        
+    }
+
+}
